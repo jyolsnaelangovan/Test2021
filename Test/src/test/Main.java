be2198ba225fb8c15b4910a970c2700215464e3d
jyolsna <jyolsna@LAPTOP-LQ5JQ10B>
2021-03-14 21:03:15 +0530

    Committing the temp branch changes

--- Test/src/test/Main.java
+++ Test/src/test/Main.java
@@ -5,6 +5,10 @@ public class Main {
 	public static void Main (String [] args) {
 		System.out.println ("Hello World");
 		System.out.println ("Git Pushed");
+		
+		for (int i=0; i<5; i++) {
+			System.out.println("Saying Hello" +i);
+		}
 	}
 
 }
